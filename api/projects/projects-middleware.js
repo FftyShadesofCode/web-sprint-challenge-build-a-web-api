7c23a8e3d9c3508ffc82f8f58e030770def16862
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-14 12:53:08 -0500

    getting things done

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,39 @@
-// add middlewares here related to projects
+//MIDDLEWARE
+const Project = require('./projects-model');
+
+//VALIDATE PROJECT ID
+async function validateID(req, res, next) {
+    try {
+        const {id} = req.params
+        const project = await Project.get(id)
+        if (!project) {
+            next({status: 404, message: 'User not found.'})
+        } else {
+            req.project = project
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'Project not found.',
+        })
+    }
+}
+
+//VALIDATE BODY TEST 10 NOT PASSING FROM HERE
+async function validateBody(req, res, next) {
+    const {name, description, completed} = req.body
+    if (!name || !description) {
+        res.status(400).json({
+            message: 'Missing name or description.'
+        })
+    } else {
+        req.name = name
+        req.description = description
+        req.completed = completed
+        next()
+    }
+
+}
+
+
+module.exports = {validateID, validateBody}
